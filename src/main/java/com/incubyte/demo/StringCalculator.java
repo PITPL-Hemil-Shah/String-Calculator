d244fb5f624668e4a6fc28ccc2deccdc18b3272b
PITPL-Hemil-Shah <PITPL-Hemil-Shah hemil@pseudocode.co>
2024-02-17 11:53:34 +0530

    Calling add with a negative number will throw an exception

--- src/main/java/com/incubyte/demo/StringCalculator.java
+++ src/main/java/com/incubyte/demo/StringCalculator.java
@@ -1,14 +1,32 @@
 package com.incubyte.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringCalculator {
 	public static int add(String numbers) {
 		if (numbers.isEmpty()) {
 			return 0;
 		}
-		String[] nums = numbers.split(",");
+		String delimiter = ",|\n";
+		if (numbers.startsWith("//")) {
+			int delimiterIndex = numbers.indexOf("\n");
+			delimiter = numbers.substring(2, delimiterIndex);
+			numbers = numbers.substring(delimiterIndex + 1);
+		}
+		String[] nums = numbers.split(delimiter);
 		int sum = 0;
+		List<Integer> negativeNumbers = new ArrayList<>();
 		for (String num : nums) {
-			sum += Integer.parseInt(num);
+			int n = Integer.parseInt(num);
+			if (n < 0) {
+				negativeNumbers.add(n);
+			} else {
+				sum += n;
+			}
+		}
+		if (!negativeNumbers.isEmpty()) {
+			throw new IllegalArgumentException("Negative numbers not allowed: " + negativeNumbers);
 		}
 		return sum;
 	}
--- src/test/java/com/incubyte/demo/StringCalculatorTest.java
+++ src/test/java/com/incubyte/demo/StringCalculatorTest.java
@@ -1,6 +1,7 @@
 package com.incubyte.demo;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
@@ -12,8 +13,32 @@ public class StringCalculatorTest {
 	}
 
 	@Test
-    public void testTwoNumbers() {
-        assertEquals(6, StringCalculator.add("1,5"));
-    }
+	public void testSingleNumber() {
+		assertEquals(1, StringCalculator.add("1"));
+	}
 
+	@Test
+	public void testTwoNumbers() {
+		assertEquals(6, StringCalculator.add("1,5"));
+	}
+
+	@Test
+	public void testNewLinesBetweenNumbers() {
+		assertEquals(6, StringCalculator.add("1\n2,3"));
+	}
+
+	@Test
+	public void testCustomDelimiter() {
+		assertEquals(3, StringCalculator.add("//;\n1;2"));
+	}
+
+	@Test
+	public void testNegativeNumbers() {
+		try {
+			StringCalculator.add("-1,2,-3");
+			fail("Expected exception for negative numbers not thrown");
+		} catch (IllegalArgumentException e) {
+			assertEquals("negative numbers not allowed: -1, -3", e.getMessage());
+		}
+	}
 }
